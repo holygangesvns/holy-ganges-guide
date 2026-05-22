f5d815d8b4d48b5a4febe38bfadc036b7557ddc5
holygangesvns <holygangesvns@gmail.com>
2026-05-22 20:34:31 +0530

    Change Link components from 'to' to 'href' prop

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="flex items-center justify-between px-6 py-4">
 
         {/* Logo + Brand */}
-        <Link to="/" className="flex items-center gap-3">
+        <Link href="/" className="flex items-center gap-3">
           <img
             src="/assets/logo.png"
             alt="Holy Ganges Dorms"
@@ -21,9 +21,9 @@ export default function Header() {
         {/* Desktop Navigation */}
         <nav className="hidden md:flex items-center gap-8 text-sm">
 
-          <Link to="/">Home</Link>
+          <Link href="/">Home</Link>
 
-          <Link to="/gallery">Gallery</Link>
+          <Link href="/gallery">Gallery</Link>
 
           <a href="/#guest-testimonials">
             Reviews
@@ -37,17 +37,17 @@ export default function Header() {
             Book Now
           </a>
 
-          <Link to="/walking-map">Location</Link>
+          <Link href="/walking-map">Location</Link>
 
-          <Link to="/street-food">Street Food</Link>
+          <Link href="/street-food">Street Food</Link>
 
-          <Link to="/treasures">Treasures</Link>
+          <Link href="/treasures">Treasures</Link>
 
-          <Link to="/aarti">Aarti</Link>
+          <Link href="/aarti">Aarti</Link>
 
-          <Link to="/backpack">Backpack</Link>
+          <Link href="/backpack">Backpack</Link>
 
-          <Link to="/support">Support</Link>
+          <Link href="/support">Support</Link>
 
         </nav>
       </div>
